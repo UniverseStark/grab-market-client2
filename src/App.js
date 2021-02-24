58f9933a2e036cbd6a2b641c06d80c0c8d18c7e9
UniverseStark <jty90k@gmail.com>
2021-02-24 17:31:30 +0900

    product css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
 import "./App.css";
+import React from "react";
 import MainPageComponent from "./main/index.js";
 import { Switch, Route } from "react-router-dom";
 import UploadPage from "./upload";
@@ -8,17 +8,25 @@ import ProductPage from "./product";
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact={true} path="/">
-          <MainPageComponent />
-        </Route>
-        <Route exact={true} path="/products/:id">
-          <ProductPage />
-        </Route>
-        <Route exact={true} path="/upload">
-          <UploadPage />
-        </Route>
-      </Switch>
+      <div id="header">
+        <div id="header-area">
+          <img src="/images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path="/">
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProductPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
